bb4e37ea2432247350f5d8268d7bd1d6e7a06ccc
Lucia <125606190+foundaxolotl@users.noreply.github.com>
2025-02-26 13:12:06 +1300

    completed get colours

--- C_03_get_all_colours.py
+++ C_03_get_all_colours.py
@@ -1,6 +1,18 @@
 import csv
 import random
 
+
+def round_ans(val):
+    """
+    Rounds temperatures to nearest degree
+    :param val: Number to be rounded
+    :return: Number rounded to nearest degree
+    """
+    var_rounded = (val * 2 + 1) // 2
+    raw_rounded = "{:.0f}".format(var_rounded)
+    return int(raw_rounded)
+
+
 # Retrieve colours from csv file and put them in a list
 file = open("00_colour_list_hex_v3.csv", "r")
 all_colors = list(csv.reader(file, delimiter=","))
@@ -20,3 +32,16 @@ while len(round_colours) < 4:
     if potential_colour[1] not in colour_scores:
         round_colours.append(potential_colour)
         colour_scores.append(potential_colour[1])
+
+print(round_colours)
+print(colour_scores)
+
+# find target score (median)
+
+# change scores to integers
+int_scores = [int(x) for x in colour_scores]
+int_scores.sort()
+
+median = (int_scores[1] + int_scores[2]) / 2
+median = round_ans(median)
+print("Median", median)
